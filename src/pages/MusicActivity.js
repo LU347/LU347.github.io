1e9e70a42c00c81a2c3179aae06cbc61a6c2f54e
LU347 <103535661+LU347@users.noreply.github.com>
2024-09-03 22:44:28 -0500

    Deleted Old Code

--- src/pages/MusicActivity.js
+++
@@ -1,9 +0,0 @@
-const MusicActivity = () => {
-    return ( 
-        <div class="container">
-            <h3>Lucile is currently listening to:</h3>
-        </div>
-     );
-}
- 
-export default MusicActivity;
