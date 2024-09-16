82e51dbf736feba88e849fc2eea2f9f43a6ac659
LU347 <103535661+LU347@users.noreply.github.com>
2024-09-15 22:27:34 -0500

    Project Cards

--- main.js
+++ main.js
@@ -35,14 +35,29 @@ function createProjectCard(data) {
     var card = document.createElement("div")
     card.classList.add("project-card")
     card.innerHTML = `
-        <img id="project-img" src=${data.image}>
-        <div id="info" class="project-info">
-            <h1>${data.name}</h1>
-            <p>${data.description}</p>
-            <ul>
-                <li>${data.tech_stack}</li>
-            </ul>
-        </div>
+        <img id="project-img" src=${data.image}>`
+
+    let projectInfo = document.createElement("div");
+    projectInfo.classList.add("project-info")
+
+    projectInfo.innerHTML = `
+        <h1>
+            <a href=${data.link}  target="_blank" rel="noreferrer noopener">${data.name} <img class="icon" src="assets/icons/github-mark-white.svg"></a>  
+        </h1>
+        <p>${data.description}</p>
     `
+    
+    let tech_list = document.createElement("ul");
+    tech_list.classList.add("tech-stack-container")
+
+    let technologies = data.tech_stack
+    technologies.forEach((tech)=> {
+        let element = document.createElement("li") //left off here
+        element.innerText = tech
+        tech_list.appendChild(element)
+    });
+    
+    projectInfo.appendChild(tech_list)
+    card.appendChild(projectInfo)
     projectsContainer.appendChild(card)
 }
