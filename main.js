579381f507c76c180ada8347d95042f6dfcde3a1
LU347 <103535661+LU347@users.noreply.github.com>
2024-09-15 20:48:21 -0500

    Project Cards

--- main.js
+++ main.js
@@ -10,3 +10,39 @@ let closeSidebarButton = document.getElementById("close-sidebar-button");
 closeSidebarButton.addEventListener("click", (e) => {
     sidebar.style.display = "none";
 })
+
+
+var projectJSON = "./lib/projects.json"
+var projectsContainer = document.getElementById("projects-container")
+
+fetch(projectJSON)
+    .then((res) => {
+        if (!res.ok) {
+            throw new Error
+                (`HTTP error! Status: ${res.status}`);
+                }
+                    return res.json();
+                })
+    .then((data) =>
+        data.forEach((obj) => {
+            createProjectCard(obj)
+        })
+    )
+    .catch((error) =>
+        console.error("Unable to fetch data:", error));
+
+function createProjectCard(data) {
+    var card = document.createElement("div")
+    card.classList.add("project-card")
+    card.innerHTML = `
+        <img id="project-img" src=${data.image}>
+        <div id="info" class="project-info">
+            <h1>${data.name}</h1>
+            <p>${data.description}</p>
+            <ul>
+                <li>${data.tech_stack}</li>
+            </ul>
+        </div>
+    `
+    projectsContainer.appendChild(card)
+}
