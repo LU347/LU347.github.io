26f7f411b76b378d640db4def0e4572c1821e0d8
LU347 <103535661+LU347@users.noreply.github.com>
2024-10-19 13:31:24 -0500

    Added a glass effect

--- personal-portfolio/src/components/Card.tsx
+++ personal-portfolio/src/components/Card.tsx
@@ -7,13 +7,17 @@ function Card(props: any) {
             translateY(${props.yOffset}rem)
             rotate(${props.rotation}deg)
         `,
-        boxShadow: `0px 10px 30px rgba(0, 0, 0, 0.285)`,
-        backgroundColor: '#d8c3ffdb'
+        //boxShadow: `0px 10px 30px rgba(0, 0, 0, 0.285)`,
+        backgroundColor: "#d8c3ffdb",
+        background: "rgba(184, 146, 255, 0.09)",
+        borderRadius: "16px",
+        boxShadow: "0 4px 30px rgba(0, 0, 0, 0.1)",
+        backdropFilter: "blur(5px)",
     };
     
     return (
         <div
-            className="bg-white m-[10px] p-[10px] pb-[25px] lg:m-[20px] lg:p-[15px] h-full w-1/2 lg:pb-[60px]"
+            className="m-[10px] p-[10px] pb-[25px] lg:m-[20px] lg:p-[15px] h-full w-1/2 lg:pb-[60px]"
             style={cardStyle}
         >
             <img src={props.imageURL} height="30vh" width="auto" alt="Card Image"></img>
