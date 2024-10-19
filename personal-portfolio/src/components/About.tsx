3a40f3e2122dc31aceb018de61f4f05110b4acf8
LU347 <103535661+LU347@users.noreply.github.com>
2024-10-18 22:45:28 -0500

    Highlighted key words in bio

--- personal-portfolio/src/components/About.tsx
+++ personal-portfolio/src/components/About.tsx
@@ -7,16 +7,15 @@ function About() {
         <CardContainer />
       </div>
       <div className="h-1/2 w-3/4 flex justify-center items-center flex-col text-center m-auto">
-        <h1 className="m-2 text-2xl lg:text-4xl font-semibold text-slate-100 font-serif">About Me</h1>
-        <div className="m-4 p-8 h-full text-md lg:text-2xl grid gap-2 text-ellipsis overflow-y-auto text-slate-400 font-sans font-light">
+        <div className="m-4 p-8 h-full text-md lg:text-2xl grid gap-2 text-ellipsis overflow-y-auto text-slate-300 font-sans font-light">
           <p>
-          From an early age, technology has always fascinated me—whether it was playing Super Mario on my Game Boy or diving into flash games online. After moving to the United States, my passion for computers truly ignited when my family gifted me a small laptop, opening up a world of possibilities.
+            <strong className="font-medium text-slate-50">From an early age, technology has always fascinated me</strong>—whether it was playing Super Mario on my Game Boy or diving into flash games online. After moving to the United States, my passion for computers truly ignited when my family gifted me a small laptop, opening up a world of possibilities.
           </p>
           <p>
-          In high school, I delved into basic electronics and got my first taste of web development through HTML and CSS, which further stoked my curiosity. Over time, this initial spark grew into a deep love for programming, as I discovered the joy of transforming a blank screen into a fully functioning app or website.
+          In high school, I delved into basic electronics and got my first taste of web development through HTML and CSS, which further stoked my curiosity. Over time, this initial spark grew into a <strong className="font-medium text-slate-50">deep love for programming</strong>, as I discovered the joy of transforming a blank screen into a fully functioning app or website.
           </p>
           <p>
-          While I explored several career paths during my time at UTSA, I eventually found my true calling in Full-Stack development. Though the journey wasn’t always linear, I’m excited about where it has led me and what the future holds.
+          While I explored several career paths during my time at UTSA, I eventually found my true calling in <strong className="font-medium text-slate-50">Full-Stack development</strong>. Though the journey wasn’t always linear, I’m excited about where it has led me and what the future holds.
           </p>
         </div>
       </div>
