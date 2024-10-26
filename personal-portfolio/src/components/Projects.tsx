c191a7834a0113c681fa16bb7932344df503f5bd
LU347 <103535661+LU347@users.noreply.github.com>
2024-10-25 22:29:35 -0500

    Update Projects.tsx

--- personal-portfolio/src/components/Projects.tsx
+++ personal-portfolio/src/components/Projects.tsx
@@ -1,8 +1,23 @@
+import test from "../assets/card-images/monsieurmignon.jpg";
 
 function Projects() {
   return (
-    <section className="h-screen grid grid-col-1">
-      <h1 className="h-full w-full flex justify-center items-center text-3xl">WIP</h1>
+    <section id="projects" className="h-screen flex flex-col justify-center align-items-center">
+      <div className="w-11/12 h-1/4 max=h-[200px] m-auto flex justify-center items-center rounded-lg border border-purple-900">
+        <div className="w-full h-full grid grid-cols-1 lg:grid-cols-2">
+          <div className="bg-red-400">Image</div>
+          <div className="bg-red-500 flex flex-col">
+            <h1>Title</h1>
+            <p>Description</p>
+            <div>
+              links
+            </div>
+          </div>
+        </div>
+
+      </div>
+
+
     </section>
   )
 }
